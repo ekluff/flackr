2b5afef43428bcc7eaa31e01804745d46929e9c1
evan clough & clayton easterbrooks <student@epicodus.com>
2015-10-21 10:58:55 -0700

    Setup database relationships.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,10 @@
 class Image < ActiveRecord::Base
+  belongs_to :user
+
+  # has_attached_file :asset, styles: { large: "500x500>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  # validates_attachment_content_type :asset, content_type: /\Aimage\/.*\Z/
+
+  validates :title, { presence: true, uniqueness: true }
+  validates :caption, { presence: true }
+  # validates :asset, presence:true
 end
